bdd84210b101296dc46a50ae9d015e3caad0e4c7
Zoly24 <alexzanderwilburn2424@outlook.com>
2026-05-19 01:53:11 -0500

    updated kitty template now there are no dark color conflicts

--- dot_config/quickshell/scripts/executable_new-theme-manager.sh
+++ dot_config/quickshell/scripts/executable_new-theme-manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-matugen image "$1" -m dark -t scheme-tonal-spot --contrast -0.1 --prefer less-saturation --lightness-dark 0.1
+matugen image "$1" -m dark -t scheme-tonal-spot --contrast 0.0 --prefer saturation --lightness-dark 0.1
 
 notify-send -a "Theme Engine" -i "$1" "Theme Applied" "Applied: Quickshell"
